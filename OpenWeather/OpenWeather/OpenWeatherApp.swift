7fa90ba5ad5dc3e3eb70e285e17168534ddd8efc
Ali Bahadoripoor <64734165+alibahadoripoor@users.noreply.github.com>
2023-07-21 01:11:04 +0200

    Initial commit

---
+++ OpenWeather/OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by Ali  on 21.07.23.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
