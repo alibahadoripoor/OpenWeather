8aeb4c9bcff21329b6f855c8a8cda67666dd3dd9
Ali <Ali.bahadoripoor@icloud.com>
2023-07-21 02:31:50 +0200

    Initial implementation of Open-Weather package

---
+++ Open-Weather/Sources/Open-Weather/Open_Weather.swift
@@ -0,0 +1,6 @@
+public struct Open_Weather {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Open-Weather/Tests/Open-WeatherTests/Open_WeatherTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Open_Weather
+
+final class Open_WeatherTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Open_Weather().text, "Hello, World!")
+    }
+}
